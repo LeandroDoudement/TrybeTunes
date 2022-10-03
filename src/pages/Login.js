6f4fd29718efce2efcd6dd79f8d46bfb6e3be91f
Leandro França <leandrodoudement@gmail.com>
2022-10-03 16:57:50 -0300

    Requisito 2 | Login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,74 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      loading: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleClick = async () => {
+    const { history } = this.props;
+    const { username } = this.state;
+    this.setState({
+      loading: true,
+    });
+    await createUser({ name: username });
+    this.setState({
+      loading: false,
+    });
+    history.push('/search');
+  };
+
   render() {
+    const { username, loading } = this.state;
+    const disableLimit = 3;
+    if (loading === true) {
+      return <Loading />;
+    }
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <form action="">
+          <label htmlFor="inputName">
+            <input
+              type="text"
+              data-testid="login-name-input"
+              name="username"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ username.length < disableLimit }
+            onClick={ this.handleClick }
+          >
+            Entrar
+
+          </button>
+        </form>
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
