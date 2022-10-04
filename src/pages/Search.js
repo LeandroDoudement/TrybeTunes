c005d1068625f91cc9300b0092cd7fe58897e97d
Leandro França <leandrodoudement@gmail.com>
2022-10-04 14:31:04 -0300

    Requisito 5 | Artist form

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,47 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      bandName: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { bandName } = this.state;
+    const buttonLimit = 2;
     return (
       <>
         <Header />
-        <div data-testid="page-search" />
+        <div data-testid="page-search">
+          <form action="">
+            <label htmlFor="bandName">
+              <input
+                type="text"
+                data-testid="search-artist-input"
+                name="bandName"
+                onChange={ this.handleChange }
+              />
+            </label>
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ bandName.length < buttonLimit }
+            >
+              Pesquisar
+
+            </button>
+          </form>
+        </div>
       </>
     );
   }
